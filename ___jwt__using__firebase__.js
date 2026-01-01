0f03504cc93517a0922d58fab091bd1c18086cdf
mokarrom17 <mokarrombasher@gmail.com>
2026-01-01 22:38:08 +0600

    jwt using firebase.js file add

---
+++ ___jwt__using__firebase__.js
@@ -0,0 +1,15 @@
+/* JWT using firebase
+
+
+1.After user login, firebase is giving us access Token
+
+2.
+fetch("", {
+headers:{
+    authorization: `Bearer ${token}`
+}
+})
+
+3.set a middleware to verify token
+
+*/
