6490647cd8bd66e427edda28fdc76a1be7097497
mokarrom17 <mokarrombasher@gmail.com>
2026-01-08 21:32:46 +0600

    add uppercase

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ const admin = require("firebase-admin");
 // const serviceAccount = require("./joblagbe-firebase-admin-key.json");
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.project_id,
-    clientEmail: process.env.client_email,
-    privateKey: process.env.private_key.replace(/\\n/g, "\n"),
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
 });
 
